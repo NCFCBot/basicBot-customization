d1c537c30e52748030570d02588e659d345ccd5c
Gamefreak0 <Gamefreak0@users.noreply.github.com>
2014-11-17 15:26:49 -0500

    Update extension.js

--- extension.js
+++ extension.js
@@ -81,6 +81,20 @@
             }
         };
 
+        bot.commands.meowCommand = {
+            command: 'meow',  //The command to be called. With the standard command literal this would be: !bacon
+            rank: 'user', //Minimum user permission to use the command
+            type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    var sounds = Array("Nya!", "Nyah!", "Nyan!");
+                    API.sendChat("/me " + sounds[Math.floor(Math.random()*items.length)]);
+                }
+            }
+        };
+
         //Load the chat package again to account for any changes
         bot.loadChat();
 
