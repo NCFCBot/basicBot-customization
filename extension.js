5cc6b447efd2cbb1b1a03c2fdaa5f6759544ab57
BrabbitX <navins1188@inbox.lv>
2015-06-21 00:26:53 +0100

    Update extension.js

--- extension.js
+++ extension.js
@@ -149,7 +149,7 @@
         cycleGuard: false,
         maximumCycletime: 10,
         timeGuard: true,
-        maximumSongLength: 8.15,
+        maximumSongLength: 5.15,
         autodisable: false,
         commandCooldown: 10,
         usercommandsEnabled: true,
