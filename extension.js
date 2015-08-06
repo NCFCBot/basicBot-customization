bbc45bd0fd12a5e5b13e901ef4401376abff7525
BrabbitX <navins1188@inbox.lv>
2015-08-06 22:27:20 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -128,6 +128,19 @@
             }
         };
 
+ bot.commands.staff = {
+            command: 'staff',  
+            rank: 'user',
+            type: 'exact',
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("@staff");
+                }
+            }
+        };
+        
         //Load the chat package again to account for any changes
         bot.loadChat();
 
