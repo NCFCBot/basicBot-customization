eb3bdba59a359c4cdd4e39278c6acbee6b65bc50
BrabbitX <navins1188@inbox.lv>
2015-08-19 15:53:28 +0100

    Update extension.js

--- extension.js
+++ extension.js
@@ -122,7 +122,7 @@
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                 if (!bot.commands.executable(this.rank, chat)) return void (0);
                 else {
-                    var sounds = Array("Nya! :cirLove: ", "Nyah! :cirLewd: ", "Nyan! :cirGlod: ");
+                    var sounds = Array("Nya! :cirLove: ", "Nyah! :cirLewd: ", "Nyan! :cirGlod: ", "Nyaaah! :nb3Gasm: ", "Nyaa! :nb3H: ");
                     API.sendChat("/me " + sounds[Math.floor(Math.random()*sounds.length)]);
                 }
             }
