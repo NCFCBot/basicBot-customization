c98ffb2e5409b34cedf8a27ad1c00a17a437548e
BrabbitX <navins1188@inbox.lv>
2015-09-09 00:40:33 +0100

    Update extension.js

--- extension.js
+++ extension.js
@@ -179,7 +179,7 @@
         ],
         afkpositionCheck: 15,
         afkRankCheck: "ambassador",
-        motdEnabled: true,
+        motdEnabled: false,
         motdInterval: 66,
         motd: " Remember to read the rules and check the blacklist info (welcome message), Also auto-join is strictly forbidden any one using it will be banned! :Rekt: " ,
         filterChat: true,
