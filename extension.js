6a8501895714c2ef45fc03053fb9f47d5d8dd013
NCFCBot <NCFCBOT@hotmail.com>
2016-07-12 20:32:57 -0400

    Update extension.js

--- extension.js
+++ extension.js
@@ -128,6 +128,21 @@
             }
         };
 
+
+        bot.commands.WoofCommand = {
+            command: 'Woof',
+            rank: 'user',
+            type: 'exact',
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    var sounds = Array("Woof! ", "Woooof! ", "HOWLLLLL! ", "Wof! ", "WOOF!! ", "SHADO Stop WOOFING ");
+                    API.sendChat("/me " + sounds[Math.floor(Math.random()*sounds.length)]);
+                }
+            }
+        };
+
          bot.commands.staff = {
             command: 'staff',  
             rank: 'residentdj',
