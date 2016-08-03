5fc059f69af3b11402961fbe40098de1cf61b707
NCFCBot <NCFCBOT@hotmail.com>
2016-08-03 22:17:42 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -441,6 +441,19 @@
             }
         };
         
+                                    bot.commands.feelsCommand = {
+            command: 'feels',
+            rank: 'user',
+            type: 'exact',
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("https://media.giphy.com/media/77jfeGVrp5mKY/giphy.gif");
+                }
+            }
+        };
+        
          bot.commands.staff = {
             command: 'staff',  
             rank: 'residentdj',
