e3fa25027a2bda1cabc8c9a427a83f4e3799037e
NCFCBot <NCFCBOT@hotmail.com>
2016-08-18 19:15:24 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -531,6 +531,19 @@
             }
         };
         
+                                    bot.commands.partyCommand = {
+            command: 'party',
+            rank: 'user',
+            type: 'exact',
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("https://media.giphy.com/media/ZRsoWfQYGpWhy/giphy.gif");
+                }
+            }
+        };
+        
         
          bot.commands.staff = {
             command: 'staff',  
