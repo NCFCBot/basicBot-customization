a678585354d34543c5472decccd2bd14153886ef
NCFCBot <NCFCBOT@hotmail.com>
2016-08-19 09:53:45 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -544,6 +544,19 @@
             }
         };
         
+                                bot.commands.profanityCommand = {
+            command: 'profanity',
+            rank: 'user',
+            type: 'exact',
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("https://memecrunch.com/meme/2JNIM/watch-your-profanity/image.png");
+                }
+            }
+        };
+        
         
          bot.commands.staff = {
             command: 'staff',  
