254fc7fe82d07287552c4f360a5589d3de255cd2
NCFCBot <NCFCBOT@hotmail.com>
2016-08-21 21:52:43 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -557,6 +557,19 @@
             }
         };
         
+                                bot.commands.karinlaughCommand = {
+            command: 'karinlaugh',
+            rank: 'user',
+            type: 'exact',
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("http://i.imgur.com/fSkcPtU.gif");
+                }
+            }
+        };
+        
         
          bot.commands.staff = {
             command: 'staff',  
