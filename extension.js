c9d91679c2fc8bda44eb9e3271e972980d59f22e
NCFCBot <NCFCBOT@hotmail.com>
2016-08-26 08:44:50 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -570,6 +570,19 @@
             }
         };
         
+                                        bot.commands.regretCommand = {
+            command: 'regret',
+            rank: 'user',
+            type: 'exact',
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("http://i.imgur.com/MKwNdPd.gif");
+                }
+            }
+        };
+        
         
          bot.commands.staff = {
             command: 'staff',  
