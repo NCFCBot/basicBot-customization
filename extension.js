f62601d33bf8bd12fbfe87e2b51a3a1ced053c24
NCFCBot <NCFCBOT@hotmail.com>
2016-08-30 11:18:07 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -700,6 +700,32 @@
             }
         };
         
+                                                bot.commands.stayawakeCommand = {
+            command: 'stayawake',
+            rank: 'user',
+            type: 'exact',
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("http://www.lovethisgif.com/uploaded_images/7174-Cute-Cartoon-Cat-Drawing-Tumblr.gif");
+                }
+            }
+        };
+        
+                                        bot.commands.coffeetimeCommand = {
+            command: 'coffeetime',
+            rank: 'user',
+            type: 'exact',
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("http://media2.giphy.com/media/687qS11pXwjCM/giphy.gif");
+                }
+            }
+        };
+        
         
          bot.commands.staff = {
             command: 'staff',  
