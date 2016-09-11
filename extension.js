d181e10d42116f06abf37c47fa02691c3582b4b6
NCFCBot <NCFCBOT@hotmail.com>
2016-09-11 22:51:55 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -934,6 +934,19 @@
             }
         };
         
+                                                bot.commands.teatimeCommand = {
+            command: 'teatime',
+            rank: 'user',
+            type: 'exact',
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("http://25.media.tumblr.com/f451844fa1c44f7e07fff34f44472cb9/tumblr_mvitntZkHM1rmhhnno1_500.gif");
+                }
+            }
+        };
+        
                                     bot.commands.competitionCommand = {
             command: 'competition',
             rank: 'user',
