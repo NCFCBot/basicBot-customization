5e5d394ebde3d8f5d3fb2c7d4bcf8a1fad9c2ab6
NCFCBot <NCFCBOT@hotmail.com>
2016-09-11 14:19:58 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -921,6 +921,19 @@
             }
         };
         
+                                                bot.commands.headpatsCommand = {
+            command: 'headpats',
+            rank: 'user',
+            type: 'exact',
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("http://i.imgur.com/aWgb4AL.gifv");
+                }
+            }
+        };
+        
                                     bot.commands.competitionCommand = {
             command: 'competition',
             rank: 'user',
