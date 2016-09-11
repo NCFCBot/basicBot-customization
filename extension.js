d1fa517c020284f25ecb547de5e7ace19c0fb83e
NCFCBot <NCFCBOT@hotmail.com>
2016-09-11 22:06:27 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -929,7 +929,7 @@
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                 if (!bot.commands.executable(this.rank, chat)) return void (0);
                 else {
-                    API.sendChat("http://i.imgur.com/aWgb4AL.gifv");
+                    API.sendChat("http://i.imgur.com/aWgb4AL.gif");
                 }
             }
         };
