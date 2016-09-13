3432760236dba1e17ae94407e5df1a97c5cfdd56
NCFCBot <NCFCBOT@hotmail.com>
2016-09-13 19:18:20 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -947,6 +947,19 @@
             }
         };
         
+                                            bot.commands.cutedevilCommand = {
+            command: 'cutedevil',
+            rank: 'user',
+            type: 'exact',
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("https://i.imgur.com/d9B0R4W.gif");
+                }
+            }
+        };
+        
                                     bot.commands.competitionCommand = {
             command: 'competition',
             rank: 'user',
