6821a6c1737424cc59ed5d796f9bf5bff21279ec
NCFCBot <NCFCBOT@hotmail.com>
2016-09-16 21:19:16 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -960,6 +960,19 @@
             }
         };
         
+                                    bot.commands.attentionCommand = {
+            command: 'attention',
+            rank: 'user',
+            type: 'exact',
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("https://67.media.tumblr.com/d83e916bcc928752478a545886881301/tumblr_nvo4cpWSRM1uh48xoo1_500.gif");
+                }
+            }
+        };
+        
                                     bot.commands.competitionCommand = {
             command: 'competition',
             rank: 'user',
