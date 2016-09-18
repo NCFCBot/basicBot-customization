d2097ffdb1ba6bc8d2a6dab784030f0231ac9fa5
NCFCBot <NCFCBOT@hotmail.com>
2016-09-18 09:06:30 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -973,6 +973,19 @@
             }
         };
         
+                                            bot.commands.petsCommand = {
+            command: 'pets',
+            rank: 'user',
+            type: 'exact',
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("http://i.imgur.com/LRDanyb.gif");
+                }
+            }
+        };
+        
                                     bot.commands.competitionCommand = {
             command: 'competition',
             rank: 'user',
