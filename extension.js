7c18c03e7c57f5f91f58a828d4329c2514bab1bb
NCFCBot <NCFCBOT@hotmail.com>
2016-10-09 21:23:36 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -1181,6 +1181,19 @@
             }
         };
         
+        
+                                               bot.commands.nope2Command = {
+            command: 'nope2',
+            rank: 'user',
+            type: 'exact',
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("https://media.giphy.com/media/USNlL9p2fxY6Q/giphy.gif");
+                }
+            }
+        };
                                     bot.commands.competitionCommand = {
             command: 'competition',
             rank: 'user',
