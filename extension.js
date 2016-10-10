fc766434b728d2a085f69e5b93a38b2d1df06e58
NCFCBot <NCFCBOT@hotmail.com>
2016-10-10 16:10:56 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -1194,6 +1194,19 @@
                 }
             }
         };
+        
+                                               bot.commands.senpaiCommand = {
+            command: 'senpai',
+            rank: 'user',
+            type: 'exact',
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("https://cdn.discordapp.com/attachments/206337582739619841/234962808096292864/200.gif");
+                }
+            }
+        };
                                     bot.commands.competitionCommand = {
             command: 'competition',
             rank: 'user',
