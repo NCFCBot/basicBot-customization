5e9cd7518bb9b423699b1116cf637e7ac79411da
NCFCBot <NCFCBOT@hotmail.com>
2016-11-11 14:05:06 +0100

    Update extension.js

--- extension.js
+++ extension.js
@@ -1415,6 +1415,19 @@
             }
         };
         
+                                                       bot.commands.complaintCommand = {
+            command: 'complaint',
+            rank: 'user',
+            type: 'exact',
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("http://i.imgur.com/eNURAHh.gif");
+                }
+            }
+        };
+        
                          bot.commands.staff = {
             command: 'staff',  
             rank: 'user',
