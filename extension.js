392431c4de9cb3b75a1a4d36ad929031d093409d
NCFCBot <NCFCBOT@hotmail.com>
2016-11-22 18:43:14 +0100

    Update extension.js

--- extension.js
+++ extension.js
@@ -1441,6 +1441,19 @@
             }
         };
         
+                                                      bot.commands.coffeeCommand = {
+            command: 'coffee',
+            rank: 'user',
+            type: 'exact',
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("http://i.imgur.com/vMLdtuj.jpg");
+                }
+            }
+        };
+        
                          bot.commands.staff = {
             command: 'staff',  
             rank: 'user',
