1768147689edd84851d86b628207d81ebdd75906
NCFCBot <NCFCBOT@hotmail.com>
2017-03-12 10:15:56 +0100

    Update extension.js

--- extension.js
+++ extension.js
@@ -1767,6 +1767,18 @@
             }
         };
         
+                                             bot.commands.wincestCommand = {
+            command: 'wincest',
+            rank: 'user',
+            type: 'exact',
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("http://i39.photobucket.com/albums/e155/teh_Itachi/bm3p8.jpg");
+                }
+            }
+        };
                                                  bot.commands.emotesCommand = {
             command: 'emotes',
             rank: 'user',
