f3ddc04596e98b7ce3c1f40ba7f4a04a2ba02229
NCFCBot <NCFCBOT@hotmail.com>
2017-04-01 00:25:22 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -1853,6 +1853,18 @@
                 }
             }
         };
+                                                bot.commands.rdjappCommand = {
+            command: 'rdjapp',
+            rank: 'user',
+            type: 'exact',
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("/me Our Resident DJ applications are now open! To apply follow the link: https://ncfc.typeform.com/to/ORo7VO");
+                }
+            }
+        };
         
                          bot.commands.staff = {
             command: 'staff',  
