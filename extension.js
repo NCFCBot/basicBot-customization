eebb3be8df2b83a892fee6b664755da4dfdfb672
NCFCBot <NCFCBOT@hotmail.com>
2017-04-13 14:54:23 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -1842,6 +1842,19 @@
             }
         };
         
+                                           bot.commands.lolidanceCommand = {
+            command: 'lolidance',
+            rank: 'user',
+            type: 'exact',
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("http://i.imgur.com/JluUY98.gif");
+                }
+            }
+        };
+        
                                                  bot.commands.emotesCommand = {
             command: 'emotes',
             rank: 'user',
