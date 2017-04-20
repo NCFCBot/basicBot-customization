af772267fa5ab6dbe82709b427d9fc46c7cc449d
NCFCBot <NCFCBOT@hotmail.com>
2017-04-20 19:30:12 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -1868,6 +1868,19 @@
             }
         };
         
+                                               bot.commands.lightsoutCommand = {
+            command: 'lightsout',
+            rank: 'user',
+            type: 'exact',
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("http://i.imgur.com/nF30Src.jpg");
+                }
+            }
+        };
+        
                                                  bot.commands.emotesCommand = {
             command: 'emotes',
             rank: 'user',
